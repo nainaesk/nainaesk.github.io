bfc93b12da60025ba460b868d78f139c576e71e7
Naina Sebastian <nainasebastian@Nainas-MacBook-Air.local>
2024-09-25 11:11:30 +0530

    services,proposal, casestudies,working process(start)

--- positivus/js/script.js
+++ positivus/js/script.js
@@ -10,3 +10,15 @@ function toggleMenu() {
         menuItems.style.opacity = 0;
     }
 }
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
